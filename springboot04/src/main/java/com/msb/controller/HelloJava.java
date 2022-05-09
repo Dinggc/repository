b0d52782fa610d372c96e6cc9f0c8a6a8abf724e
dinggc <1351078450@qq.com>
2022-05-09 12:36:27 +0800

    提交springboot04

---
+++ springboot04/src/main/java/com/msb/controller/HelloJava.java
@@ -0,0 +1,15 @@
+package com.msb.controller;
+
+/**
+ * @Auther:GDC
+ * @Date:2022/5/9 - 12:33
+ * @Description:com.msb.controller
+ * @version:1.0
+ */
+public class HelloJava
+{
+    public static void main(String[] args) {
+        String str = "HELLO WORLD";
+        System.out.println(str);
+    }
+}
